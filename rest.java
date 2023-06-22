d6409f14a94439f092933320cc17f9ff24339226
anilkumar <anilkumar@gmail.com>
2023-06-22 12:19:05 +0530

    commiting rest to copy1

---
+++ rest.java
@@ -0,0 +1,10 @@
+public class Rest{
+    int a=10;
+    public static void main(String[] args) {
+        System.err.println("Hello-World");
+
+        Rest rest =new Rest();
+        System.out.println(rest.a);
+
+    }
+} 
